546a8486aeab76d817c2d457ef75a12061d16a85
Ruterana <ncharlene38@gmail.com>
2019-09-05 16:26:07 +0200

    i create test file

--- user_account.py
+++ user_account.py
@@ -4,3 +4,28 @@ class Accounts:
     """
 
     pass
+def __init__(self,User_name,password,first_name,last_name,phone_number,email):
+
+        '''
+        __init__ method that helps us define properties for our objects or instances.
+        ''' 
+        Args:
+            User_name:new Accounts user_name.
+            password:new password.
+            first_name: New Acconts first name.
+            last_name : New  Accounts last name.
+            number: New Accounts phone number.
+            email : New Accounts email address.
+
+
+def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+        self.User_name=User_name
+        self.password=password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+         
---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from User_account import Accounts # Importing the contact class
+
+class TestAccounts(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Accounts class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
