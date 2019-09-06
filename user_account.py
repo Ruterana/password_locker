8c626d117004b1e400c7f32eed202b4d3eea912d
Ruterana <ncharlene38@gmail.com>
2019-09-06 08:41:09 +0200

    i make first runand their test

--- user_account.py
+++ user_account.py
@@ -1,31 +1,24 @@
-class Accounts:
+class Account:
     """
-    Class that generates new instances of Accounts
+    Class that generates new instances of Accounts.
     """
 
-    pass
-def __init__(self,User_name,password,first_name,last_name,phone_number,email):
+    user_account_list = []
 
-        '''
-        __init__ method that helps us define properties for our objects or instances.
-        ''' 
-        Args:
-            User_name:new Accounts user_name.
-            password:new password.
-            first_name: New Acconts first name.
-            last_name : New  Accounts last name.
-            number: New Accounts phone number.
-            email : New Accounts email address.
-
-
-def __init__(self,first_name,last_name,phone_number,email):
+    def __init__(self,user_name,password):
 
       # docstring removed for simplicity
-        self.User_name=User_name
-        self.password=password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = number
-        self.email = email
 
-         
+        self.user_name = user_name
+        self.password = password
+# def save_Account(self):
+
+#         '''
+#         save_Account method saves Account objects into Account_list
+#         '''
+
+#         Account.Account_list.append(self)
+
+        
+
+
--- user_test.py
+++ user_test.py
@@ -1,11 +1,44 @@
-import unittest # Importing the unittest module
-from User_account import Accounts # Importing the contact class
 
-class TestAccounts(unittest.TestCase):
+import unittest # Importing the unittest module
+from user_account import Account 
+
+class TestAccount(unittest.TestCase):
+
+
 
     '''
-    Test class that defines test cases for the Accounts class behaviours.
+    Test class that defines test cases for the account class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_Account = Account("charlene","pass12!@") # create Account object
+
+    def tearDown(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        ''' 
+        Account.user_account_list=[]
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_Account.user_name,"charlene")
+        self.assertEqual(self.new_Account.password,"pass12!@")
+#########test2###############
+    # def test_save_Account(self):
+    #     '''
+    #     test_save_Account test case to test if the Account object is saved into
+    #      the Account list
+    #     '''
+    #     self.new_Account.save_Account() # saving the new Account
+    #     self.assertEqual(len(Account.Account_list),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
